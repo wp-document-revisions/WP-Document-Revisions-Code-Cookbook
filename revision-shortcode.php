6a40f25b392be664b459cb08b49409c34083e6e3
Benjamin J. Balter <ben@balter.com>
2011-08-25 12:23:43 -0400

    revision shortcode

---
+++ revision-shortcode.php
@@ -0,0 +1,49 @@
+<?php
+/*
+Plugin Name: WP Document Revisions - Revision Shortcode
+Plugin URI: http://
+Description: Code sample to demonstrate short code to list revisions
+Version: 1.0
+Author: Benjamin J. Balter
+Author URI: http://ben.balter.com
+License: GPL2
+*/
+
+/**
+ * Callback to display revisions
+ * @param $atts array attributes passed via short code
+ * @returns string a UL with the revisions
+ */
+function wpdr_shotcode( $atts ) {
+
+	//extract args
+	extract( shortcode_atts( array(
+		'id' => null,
+	), $atts ) );
+	
+	//get WPDR object	
+	$wpdr = Document_Revisions::$instance;
+	$revisions = $wpdr->get_revisions( $id );
+	
+	//buffer output to return rather than echo directly
+	ob_start();
+	?>
+	<ul class="revisions">
+	<?php 
+	//loop through each revision
+	foreach ( $revisions as $revision ) { ?>
+		<li>
+			<a href="<?php echo get_permalink( $revision->ID ); ?>" title="<?php echo $revision->post_date; ?>" class="timestamp" id="<?php echo strtotime( $revision->post_date ); ?>">
+			<?php echo human_time_diff( strtotime( $revision->post_date ), current_time('timestamp') ); ?>
+			</a> by <?php echo get_the_author_meta( 'display_name', $revision->post_author ); ?>
+		</li>
+	<?php } ?>
+	</ul>
+	<?php
+	//grab buffer contents and clear
+	$output = ob_get_contents();
+	ob_end_clean();
+	return $output;
+}
+
+add_shortcode( 'reisions', 'wpdr_shortcode' );
