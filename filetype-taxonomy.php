8bb2759167882a99de1161a2425eb941c55c3119
Benjamin J. Balter <ben@balter.com>
2011-09-06 00:53:54 -0400

    filetype filter

---
+++ filetype-taxonomy.php
@@ -0,0 +1,37 @@
+<?php
+/*
+Plugin Name: WP Document Revisions - Filtering by Filetype
+Plugin URI: https://github.com/benbalter/WP-Document-Revisions-Code-Cookbook
+Description: Code sample to demonstrate how to build a filter by filetype taxonomy
+Version: 1.0
+Author: Benjamin J. Balter
+Author URI: http://ben.balter.com
+License: GPL2
+*/
+
+function wpdr_register_filetype_taxonomy() {
+	
+	// better way to do this would be with WP Document Revisions Custom Taxonomy Generator
+	// http://wordpress.org/extend/plugins/wp-document-revisions-custom-taxonomy-and-field-generator/
+
+	register_taxonomy( 'filetype', 'document', array(
+		'label' => __('Filetypes'),
+		'show_ui' => true,
+		'public' => false,
+	));
+
+}
+
+add_action( 'init', 'wpdr_register_filetype_taxonomy' );
+
+function wpdr_update_type( $postID ) {
+
+	$wpdr = Document_Revisions::$instance;
+	
+	if ( !$wpdr->verify_post_type( $postID ) )
+		return;
+		
+	wp_set_post_terms( $postID, array( $wpdr->get_extension( $postID ) ), 'filetype', false );
+}
+
+add_action( 'save_post', 'wpdr_update_type' );
