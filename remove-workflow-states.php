202a5107ec27b2e32542566d798e61cd30b23dc3
Benjamin J. Balter <ben@balter.com>
2011-09-25 12:43:52 -0400

    check for wpdr before removing ws

--- remove-workflow-states.php
+++ remove-workflow-states.php
@@ -1,21 +1,26 @@
 <?php
 /*
-Plugin Name: WP Document Revisions - Remove Workflow State
-Plugin URI: https://github.com/benbalter/WP-Document-Revisions-Code-Cookbook
+Plugin Name: Remove Workflow State
+Plugin URI: 
 Description: Removes Workflow State Taxonomy from Documents
-Version: 1.0
 Author: Benjamin J. Balter
 Author URI: http://ben.balter.com
 License: GPL2
 */
 
 function wpdr_remove_workflow_init_hooks() {
+	if ( !class_exists( 'Document_Revisions' ) )
+		return;
+
 	$wpdr = Document_Revisions::$instance;
 	remove_action( 'admin_init', array( &$wpdr, 'initialize_workflow_states' ) );
 	remove_action( 'init', array( &$wpdr, 'register_ct' ) );
 }
 
 function wpdr_remove_workflow_admin_hooks() {
+	if ( !class_exists( 'Document_Revisions' ) )
+		return;
+
 	$wpdr = Document_Revisions::$instance;
 	remove_filter( 'manage_edit-document_columns', array( &$wpdr, 'add_workflow_state_column' ) );
 	remove_action( 'manage_document_posts_custom_column', array( &$wpdr, 'workflow_state_column_cb' ) );
