2ccccb5b492fe8965ce64601ed08ad24d6b98c48
Benjamin J. Balter <ben@balter.com>
2011-10-20 10:48:26 -0400

    added support for post meta

--- wpdr-edit-flow.php
+++ wpdr-edit-flow.php
@@ -12,7 +12,7 @@ License: GPL2
 
 function wpdr_ef_cpt_filter( $cpt ) {
 
-	$cpt['supports']  = array_merge( $cpt['supports'], array( 'ef_custom_statuses', 'ef_editorial_comments', 'ef_notifications' ) );
+	$cpt['supports']  = array_merge( $cpt['supports'], array( 'ef_custom_statuses', 'ef_editorial_comments', 'ef_notifications', 'ef_editorial_metadata' ) );
 	
 	return $cpt;
 }
